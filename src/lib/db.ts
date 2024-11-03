35cded1b5bece84ea39c94573e98f1bd7bd7da29
Den15k4 <118001931+Den15k4@users.noreply.github.com>
2024-11-04 00:09:29 +0500

    Create db.ts

---
+++ src/lib/db.ts
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+
+let pool: Pool;
+
+if (!pool) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+}
+
+export { pool };
